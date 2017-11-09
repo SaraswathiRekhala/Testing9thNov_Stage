ca010487ece0a3aad835558d131cd9ae8dce7d83
SaraswathiRekhala <saraswathi.rekhala+11@wavemaker.com>
2017-11-09 05:21:30 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Testing9thNov @Stage",
+  "homePage" : "Main",
+  "name" : "Testing9thNov_Stage",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
